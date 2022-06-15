54ac175e0e53e49b56daccf550a57ba92a0711c3
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2022-06-16 00:51:36 +0200

    Add Channel Object

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -6,11 +6,60 @@
 /*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 12:19:33 by jberredj          #+#    #+#             */
-/*   Updated: 2022/06/08 11:23:27 by jberredj         ###   ########.fr       */
+/*   Updated: 2022/06/16 00:39:03 by jberredj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHANNEL_HPP
 # define CHANNEL_HPP
+# include "User.hpp"
+# include <map>
+# include <vector>
+# include <string>
+
+class Server;
+
+class Channel
+{
+public:
+	Channel(void);
+	Channel(const Channel &src);
+	~Channel(void);
+
+	Channel	&operator=(const Channel &rhs);
+
+	bool	setUserLimit(int limit);
+	bool	setUserMode(std::string mode, User* user);
+	void	setChannelMode(std::string mode);
+	bool	addUser(User *user);
+	bool	removeUser(User *user);
+	void	broadcastMessage(std::string message);
+	bool	isBanned(User *user);
+	void	setInvite(bool invite);
+	bool	getInvite(void) const;
+	bool	isInvited(User* user);
+
+	std::string	getName(void) const;
+	int			getUserLimit(void) const;
+	std::string	getUserMode(User* user);
+	std::string	getChannelMode(void) const;
+private:
+	std::string						_name;
+	int								_userLimit;
+	int								_nbrMember;
+	std::string						_mode;
+	std::vector<User*>				_member;
+	bool							_inviteOnly;
+	std::vector<User*>				_inviteList;
+	std::vector<User*>				_banList;
+	std::map<User*, std::string>	_userModes;
+
+	bool	_banUser(User* user);
+	bool	_unbanUser(User* user);
+	bool	_inviteUser(User* user);
+	bool	_revokeInviteUser(User* user);
+};
+
+
 
 #endif
--- srcs/Channel/Channel.cpp
+++ srcs/Channel/Channel.cpp
@@ -6,7 +6,163 @@
 /*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 09:07:05 by jberredj          #+#    #+#             */
-/*   Updated: 2022/06/08 11:23:14 by jberredj         ###   ########.fr       */
+/*   Updated: 2022/06/16 00:50:30 by jberredj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Channel.hpp"
+#include "typedefs.hpp"
+#include <algorithm>
+
+Channel::Channel(void):
+_name("DEFAULT"), _userLimit(-1), _nbrMember(0), _mode(""), _member(), _inviteOnly(false), _inviteList(), _banList(),
+_userModes()
+{
+}
+
+Channel::Channel(const Channel &src):
+_name(src._name), _userLimit(src._userLimit), _nbrMember(src._nbrMember), _mode(src._mode), _member(src._member),
+_inviteOnly(src._inviteOnly), _inviteList(src._inviteList), _banList(src._banList), _userModes(src._userModes)
+{
+}
+
+Channel::~Channel(void)
+{
+}
+
+Channel	&Channel::operator=(const Channel &rhs)
+{
+	if (this != &rhs)
+	{
+		_name = rhs._name;
+		_userLimit = rhs._userLimit;
+		_nbrMember = rhs._nbrMember;
+		_mode = rhs._mode;
+		_member = rhs._member;
+		_inviteOnly = rhs._inviteOnly;
+		_inviteList = rhs._inviteList;
+		_banList = rhs._banList;
+		_userModes = rhs._userModes;
+	}
+	return *this;
+}
+
+bool	Channel::setUserLimit(int limit)
+{
+	if (limit < 0)
+		return false;
+	_userLimit = limit;
+	return true;
+}
+bool	Channel::setUserMode(std::string mode, User* user)
+{
+	if (!user)
+		return false;
+	std::map<User*, std::string>::iterator it;
+	for (it = _userModes.begin(); it != _userModes.end(); it++)
+		if ((*it).first == user)
+			break;
+	if (it == _userModes.end())
+		return false;
+	(*it).second = mode;
+	return true;
+
+}
+
+void	Channel::setChannelMode(std::string mode) {_mode=mode;}
+
+bool	Channel::addUser(User *user)
+{
+	if (!user)
+		return false;
+	if (_nbrMember == _userLimit)
+		return false;
+	if (isBanned(user))
+		return false;
+	if (_inviteOnly && !isInvited(user))
+		return false;
+	_member.push_back(user);
+	_userModes.insert(std::make_pair(user, ""));
+	_nbrMember++;
+	return true;
+}
+
+bool	Channel::removeUser(User *user)
+{
+	if (!user)
+		return false;
+	std::vector<User*>::iterator it = std::find(_member.begin(), _member.end(), user);
+	if (it == _member.end())
+		return false;
+	_member.erase(it);
+	_userModes.erase(user);
+	return true;
+}
+void	Channel::broadcastMessage(std::string message)
+{
+	for (std::vector<User*>::iterator it = _member.begin(); it != _member.end(); it++)
+		(*it)->addReply(message);
+}
+
+std::string	Channel::getName(void) const {return _name;}
+int			Channel::getUserLimit(void) const {return _userLimit;}
+std::string	Channel::getUserMode(User* user)
+{
+	std::map<User*, std::string>::iterator it;
+	for (it = _userModes.begin(); it != _userModes.end(); it++)
+		if ((*it).first == user)
+			break;
+	if (it == _userModes.end())
+		return "";
+	return _userModes[user];
+}
+std::string	Channel::getChannelMode(void) const {return _mode;}
+
+bool	Channel::isBanned(User *user)
+{
+	std::vector<User*>::iterator it = find(_banList.begin(), _banList.end(), user);
+	if (it != _banList.end())
+		return true;
+	return false;
+}
+
+void	Channel::setInvite(bool invite) {_inviteOnly = invite;}
+bool	Channel::getInvite(void) const {return _inviteOnly;}
+
+bool	Channel::isInvited(User* user)
+{
+	std::vector<User*>::iterator it = find(_inviteList.begin(), _inviteList.end(), user);
+	if (it != _inviteList.end())
+		return true;
+	return false;
+}
+
+bool	Channel::_banUser(User* user)
+{
+	if (isBanned(user))
+		return false;
+	_banList.push_back(user);
+	return true;
+}
+
+bool	Channel::_unbanUser(User* user)
+{
+	if (!isBanned(user))
+		return false;
+	_banList.erase(find(_banList.begin(), _banList.end(), user));
+	return true;
+}
+bool	Channel::_inviteUser(User* user)
+{
+	if (isInvited(user))
+		return false;
+	_inviteList.push_back(user);
+	return true;
+}
+bool	Channel::_revokeInviteUser(User* user)
+{
+	if (!isInvited(user))
+		return false;
+	_inviteList.erase(find(_inviteList.begin(), _inviteList.end(), user));
+	return true;
+}
